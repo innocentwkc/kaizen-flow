c067c619e22d6997ca77909683f084a3a3ef8cda
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-09-22 21:23:16 +0200

    add tailwind library to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+
+// import './assets/css/main.css'
+import './assets/css/tailwind.css' // Import taild css file
+
 import router from './router'
 
 const app = createApp(App)
