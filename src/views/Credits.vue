1c0ffb8a035b3dc819cc4981c448740cc3d1fc88
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-09-22 20:54:48 +0200

    modify favicon and add credits page

---
+++ src/views/Credits.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <a target="_blank" href="https://icons8.com/icon/pLiaaoa41R9n/wind">Favicon Wind Icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+  </div>
+</template>
