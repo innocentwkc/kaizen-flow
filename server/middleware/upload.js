f5df7d39d4d4798eba25fda127e18605d033b75a
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-09-22 23:24:41 +0200

    update comments
    update comments using Javadoc syntax for the server.js file and upload.js middleware

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,18 +1,56 @@
+/**
+ * Module for handling file uploads using Multer middleware.
+ * The uploaded files are stored in a specified destination with unique filenames.
+ * @module uploadMiddleware
+ */
+
 const multer = require('multer');
 const path = require('path');
 
+/**
+ * Multer disk storage configuration for file uploads.
+ * @const {object}
+ */
 const storage = multer.diskStorage({
+  /**
+   * Specifies the destination folder for storing uploaded files.
+   * @param {object} req - Express request object.
+   * @param {object} file - Uploaded file object.
+   * @param {function} cb - Callback function to set the destination path.
+   * @returns {void}
+   */
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, '../data/uploads'));
   },
+
+  /**
+   * Generates a unique filename for the uploaded file.
+   * @param {object} req - Express request object.
+   * @param {object} file - Uploaded file object.
+   * @param {function} cb - Callback function to set the filename.
+   * @returns {void}
+   */
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); // TODO: use username custom file name
     cb(null, 'pdf-' + uniqueSuffix + path.extname(file.originalname));
   }
 });
 
+/**
+ * Multer middleware configuration for handling file uploads.
+ * @const {object}
+ */
 const upload = multer({ storage });
 
+/**
+ * Middleware function to handle single file uploads named 'pdf'.
+ * @function
+ * @name uploadMiddleware
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {function} next - Express next middleware function.
+ * @returns {void}
+ */
 const uploadMiddleware = upload.single('pdf');
 
 module.exports = uploadMiddleware;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
+/**
+ * This script sets up an Express server to handle API requests for PDF file uploads.
+ * It utilizes middleware for handling file uploads and employs CORS for cross-origin requests.
+ */
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const uploadMiddleware = require('./middleware/upload');
 
 const app = express();
+
+// The port on which the server will listen for incoming requests
 const PORT = 3020;
 
+// Enable Cross-Origin Resource Sharing (CORS) for all routes
 app.use(cors());
+
+// Parse incoming JSON requests
 app.use(bodyParser.json());
+
+// Parse URL-encoded requests
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse incoming JSON requests
 app.use(express.json());
 
 const router = express.Router();
 
-// API endpoint to handle PDF uploads
+/**
+ * API endpoint to handle PDF file uploads.
+ *
+ * @param {string} '/upload' - The route for handling file uploads.
+ * @param {function} uploadMiddleware - Middleware function for handling file uploads.
+ * @param {function} (req, res) - Callback function to handle the POST request.
+ * @returns {object} - JSON response indicating success or failure of the upload.
+ */
 router.post('/upload', uploadMiddleware, (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No PDF file uploaded' });
@@ -23,8 +43,16 @@ router.post('/upload', uploadMiddleware, (req, res) => {
   res.json({ message: 'PDF file uploaded successfully', filename: req.file.filename });
 });
 
+// Mount the router at the '/api' path
 app.use('/api', router);
 
+/**
+ * Start the Express server and listen for incoming connections on the specified port.
+ *
+ * @param {number} PORT - The port number on which the server will listen for requests.
+ * @param {function} () - Callback function to be executed once the server starts.
+ * @returns {void}
+ */
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
