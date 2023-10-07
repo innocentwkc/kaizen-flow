54fa54b65c08d676d4293213d6dc497482ab9e81
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-10-07 20:33:01 +0200

    edit menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,35 +14,35 @@
         </li>
         <!-- border-l-4 border-black mb-2 -->
         <li class="border-l-4 border-black mb-2">
-          <a href="#" class="py-2 px-3 inline-block w-full h-full font-bold">
+          <router-link to="#" class="py-2 px-3 inline-block w-full h-full font-bold">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
             Dashboard
-          </a>
+          </router-link>
         </li>
         <li class="mb-2">
-          <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
+          <router-link to="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
             Calender
-          </a>
+          </router-link>
         </li>
         <li class="mb-2">
-          <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
+          <router-link to="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
-            Modules/Syllabi
-          </a>
+            Popular Syllabi
+          </router-link>
         </li>
-        <li class="mb-2">
+        <!-- <li class="mb-2">
           <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
             Uploads
           </a>
-        </li>
-        <li class="mb-2">
+        </li> -->
+        <!-- <li class="mb-2">
           <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
             Settings
           </a>
-        </li>
+        </li> -->
       </ul>
     </div>
   </div>
