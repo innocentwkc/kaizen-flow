f382865415772688e887c0aae40a907208c3d083
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-10-16 13:47:15 +0200

    rename popular view to saved

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,12 +16,12 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/popular',
-      name: 'popular',
+      path: '/saved',
+      name: 'saved',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Popular.vue')
+      component: () => import('../views/Saved.vue')
     },
     {
       path: '/calender',
