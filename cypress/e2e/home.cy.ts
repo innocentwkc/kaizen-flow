bc2ca7032f838ff3bcf6bd17cc5b71568d412678
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-11-30 20:43:41 +0200

    update cypress e2e tests

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,30 @@
+describe('Home Page Test', () => {
+  beforeEach(() => {
+    // Visit the home page before each test
+    cy.visit('/');
+  });
+
+  it('has a working search input', () => {
+    cy.get('input[type="search"]').should('exist');
+    cy.get('input[type="search"]').type('Sample search{enter}');
+    // Add assertions based on how your app handles search input
+  });
+
+  it('has a navigation sidebar with links', () => {
+    cy.get('nav').should('exist');
+    cy.get('a[href="/"]').should('contain', 'Dashboard');
+    cy.get('a[href="/calender"]').should('contain', 'Calender');
+    cy.get('a[href="/uploads"]').should('contain', 'Uploads');
+    // Continue with other links as needed
+  });
+
+  it('contains a main section with a form to upload PDF', () => {
+    cy.get('main').should('exist');
+    cy.get('main').find('form').should('exist');
+    cy.get('input[type="file"]').should('exist');
+    cy.get('input[type="file"]').should('have.attr', 'accept', '.pdf');
+    cy.get('button[type="submit"]').should('contain', 'Upload PDF');
+  });
+
+  // Add more tests as needed for other elements or interactions
+});
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="app-navigation flex">
-    <div class="flex-1 h-screen bg-gray-200 py-2 px-0.5 pr-2">
+    <nav class="flex-1 h-screen bg-gray-200 py-2 px-0.5 pr-2">
       <ul>
         <li class="relative mb-6">
-          <!-- <input type="search" class="w-full rounded shadow py-2 px-3 outline-none focus:shadow-outline focus:bg-gray-200" placeholder="Search"> -->
-          <!-- <div class="relative text-black-600 focus-within:text-black-700" /> -->
           <span class="absolute inset-y-0 left-0 flex items-center pl-2">
             <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
               <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-4 h-4"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -12,21 +10,12 @@
           </span>
           <input type="search" name="q" class="w-full rounded shadow py-2 px-3 pl-12 outline-none focus:shadow-outline focus:bg-black-200" placeholder="Search..." autocomplete="off">
         </li>
-        <!-- border-l-4 border-black mb-2 -->
         <li class="border-black mb-2" :class="{ 'border-l-4': isActive('dashboard') }">
           <router-link to="/" class="py-2 px-3 inline-block w-full h-full font-bold" @click="setActiveLink('dashboard')">
             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
             Dashboard
           </router-link>
         </li>
-        <!-- <li class="border-black mb-2" :class="{ 'border-l-4': isActive('recent') }">
-          <router-link to="/recent" class="py-2 px-3 inline-block w-full h-full text-black font-bold" @click="setActiveLink('recent')">
-             <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.5" viewBox="0 0 24 24" stroke="currentColor">
-              <path id="Shape" d="M9.682,18.75a.75.75,0,0,1,.75-.75,8.25,8.25,0,1,0-6.189-2.795V12.568a.75.75,0,0,1,1.5,0v4.243a.75.75,0,0,1-.751.75H.75a.75.75,0,0,1,0-1.5H3a9.75,9.75,0,1,1,7.433,3.44A.75.75,0,0,1,9.682,18.75Zm2.875-4.814L9.9,11.281a.754.754,0,0,1-.22-.531V5.55a.75.75,0,1,1,1.5,0v4.889l2.436,2.436a.75.75,0,1,1-1.061,1.06Z" transform="translate(1.568 2.25)" fill="#141124"/>
-            </svg>
-            Recent
-          </router-link>
-        </li> -->
         <li class="border-black mb-2" :class="{ 'border-l-4': isActive('calender') }">
           <router-link to="/calender" class="py-2 px-3 inline-block w-full h-full text-black font-bold" @click="setActiveLink('calender')">
             <!-- <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg> -->
@@ -41,37 +30,16 @@
             </svg>
             Uploads
           </router-link>
-        </li>
-        <!-- <li class="border-black mb-2" :class="{ 'border-l-4': isActive('timer') }">
-          <router-link to="/timer" class="py-2 px-3 inline-block w-full h-full text-black font-bold" @click="setActiveLink('timer')">
-            <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 22C16.4183 22 20 18.4183 20 14C20 9.58172 16.4183 6 12 6C7.58172 6 4 9.58172 4 14C4 18.4183 7.58172 22 12 22Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M9 2H15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-              <path d="M15.24 10.76L12 14" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-            Timer
-          </router-link>
-        </li> -->
-        <!-- <li class="mb-2">
-          <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
-            <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
-            Uploads
-          </a>
-        </li> -->
-        <!-- <li class="mb-2">
-          <a href="#" class="py-2 px-3 inline-block w-full h-full text-black font-bold">
-            <svg class="inline h-6 w-6 -mt-1 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-            Settings
-          </a>
-        </li> -->
+        </li>     
       </ul>
-    </div>
+    </nav>
   </div>
 </template>
 
 <script setup>
   import { ref, watchEffect } from 'vue';
   import { useRoute } from 'vue-router'
+
   import CalenderIcon from './icons/CalenderIcon.vue'
 
 
@@ -107,14 +75,6 @@
   watchEffect(() => {
     activeLink.value = route.name
   })
-
-  // onMounted(() => {
-  //   activeLink.value = route.name
-
-  //   console.log(route.name)
-  //   console.log(activeLink.value)
-
-  // })
 
 </script>
 
